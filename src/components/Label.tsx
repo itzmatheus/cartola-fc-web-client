ebc96124c20c2b364ddd8fd473e203129f2063e2
Matheus Leite <matheusleite@Matheuss-MacBook-Air.local>
2023-01-05 22:31:25 -0300

    feat: customize home page

---
+++ src/components/Label.tsx
@@ -0,0 +1,10 @@
+import { Typography, TypographyProps } from "@mui/material"
+
+
+export type LabelProps = TypographyProps;
+
+export const Label = (props: TypographyProps) => {
+    return (
+        <Typography variant="h6" component='span' {...props} />
+    )
+}
